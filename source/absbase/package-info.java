4f779c204c94c12d22dbf686f59936d44f478326
GraysColour <99109424+GraysColour@users.noreply.github.com>
2022-04-22 17:51:50 +0200

    javaDoc for package absbase

---
+++ source/absbase/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Holds one abstract class {@link absbase.DayX}.
+ *
+ * <p> Extend this class when implementing a Day.
+ *
+ * @author  GraysColour
+ * @version 1.0
+ * @since   1.0
+ */
+
+package absbase;
