087f67acaae13a2a842466c122ee3dabe92bfb55
GraysColour <99109424+GraysColour@users.noreply.github.com>
2022-04-25 12:13:01 +0200

    AllDaysI javaDoc

--- source/base/AllDaysI.java
+++ source/base/AllDaysI.java
@@ -1,7 +1,46 @@
 package base;
 
+/**
+ * The interface for the all-day runner {@link AllDays} in the unnamed/default package.
+ *
+ * @author  GraysColour
+ * @version 1.0
+ * @since   1.0
+ */
+
 public interface AllDaysI {
+  /**
+   * Used by {@link util.CommandLineOptions}.
+   *
+   * @param printTime whether to print the runtime.
+   * <code>false</code> will not print the runtimes.
+   */
   void setPrintTime(boolean printTime);
+
+  /**
+   * Used by {@link util.CommandLineOptions}.
+   *
+   * <p> Sets the command line arguments for running each day.
+   * <p> For example:
+   * <pre>
+   *     setArgs(new String[]{"-d", "challenge"})</pre>
+   *
+   * @param dayArgs command line arguments for running the days.
+   */
   void setArgs(String[] dayArgs);
+
+  /**
+   * Used by {@link util.CommandLineOptions}.
+   *
+   * <p> Sets the package names for discovering YearYYYY packages, where YYYY is a 4 digit year.
+   * <p> For example:
+   * <pre>
+   *     setPackageNames("2021")</pre>
+   * or
+   * <pre>
+   *     setPackageNames("2021, 2022")</pre>
+   *
+   * @param years a {@link String} of comma separated 4 digit years
+   */
   void setPackageNames(String years);
 }
