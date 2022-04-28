1ac9b98ee0071bad26d0022042e3d1ec78313b09
GraysColour <99109424+GraysColour@users.noreply.github.com>
2022-04-28 11:29:03 +0200

    StateI javaDoc

--- source/base/StateI.java
+++ source/base/StateI.java
@@ -1,7 +1,34 @@
 package base;
 
+/**
+ * The interface for {@link util.State} which is used
+ * to determine the {@link util.Result#state state} of a {@link util.Result}
+ *
+ * @author  GraysColour
+ * @version 1.0
+ * @since   1.0
+ */
+
 public interface StateI {
+
+  /**
+   * Used by {@link util.Printers}.
+   *
+   * @return A {@link String} message expected to be printed.
+   */
   String getMessage();
+
+  /**
+   * Used by {@link util.Printers}.
+   *
+   * @return An {@link Exception} expected to be printed.
+   */
   Exception getException();
+
+  /**
+   * Used by {@link util.Printers}.
+   *
+   * @return {@link base.Status} expected to be evaluated.
+   */
   Status getStatus();
 }
