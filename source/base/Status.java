5bdb4807da23b33ddd0806fba9beb8ed8ecc7ac3
GraysColour <99109424+GraysColour@users.noreply.github.com>
2022-05-02 11:36:17 +0200

    Status javaDoc

--- source/base/Status.java
+++ source/base/Status.java
@@ -1,11 +1,64 @@
 package base;
 
+/**
+ * Enum to determine the status of a {@link base.StateI}.
+ *
+ * @author  GraysColour
+ * @version 1.0
+ * @since   1.0
+ */
+
 public enum Status {
+  /**
+   * Normal long result
+   */
   NORMAL_LONG,
+
+  /**
+   * AscII result.
+   *
+   * <p> <i>Note: There may be a long of <code>0L</code>, but it's not representative.</i>
+   */
   ASCII,
+
+  /**
+   * There is no result.
+   *
+   * <p> Signals a dummy {@link base.ResultI}
+   *
+   * <p> <i>Note: There may be a long of <code>0L</code>, but it's not representative.</i>
+   */
   NO_RESULT,
+
+  /**
+   * Reading the file did not cause errors.
+   *
+   * <p> This is an intermediate {@link Status}.
+   * <p> <i>Note: There may be a long of <code>0L</code>, but it's not representative.</i>
+   */
   FILE_OK,
+
+  /**
+   * File cannot be found.
+   *
+   * <p> This is an error {@link Status}.
+   * <p> <i>Note: There may be a long of <code>0L</code>, but it's not representative.</i>
+   */
   NO_FILE,
+
+  /**
+   * File has been found, but it's empty.
+   *
+   * <p> This is an error {@link Status}.
+   * <p> <i>Note: There may be a long of <code>0L</code>, but it's not representative.</i>
+   */
   NO_FILE_CONTENT,
+
+  /**
+   * An exception occurred.
+   *
+   * <p> This is an error {@link Status}.
+   * <p><i>Note: There may be a long of <code>0L</code>, but it's not representative.</i>
+   */
   GOT_EXCEPTION;
 }
