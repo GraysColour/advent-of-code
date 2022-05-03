3123b2089cd8b18275b64a10c9db1befc11317c1
GraysColour <99109424+GraysColour@users.noreply.github.com>
2022-05-03 07:34:49 +0200

    javaDoc for package base

---
+++ source/base/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Collection of interfaces.
+ *
+ * <p>The aim of the package is to decouple concrete implementations as parameters.
+ *
+ * @author  GraysColour
+ * @version 1.0
+ * @since   1.0
+ */
+
+package base;
